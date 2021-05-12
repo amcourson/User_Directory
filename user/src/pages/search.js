6361ab44694acf150d6adda6c05cb5d561805b40
amcourson <amcourson09@sbcglobal.net>
2021-05-11 19:31:55 -0500

    starting imports

---
+++ user/src/pages/search.js
@@ -0,0 +1,6 @@
+import React, { Component } from "react";
+import API from "../utils/API";
+import container from "../components/Container";
+import searchForm from "../components/SearchForm";
+import searchResults from "../components/SearchResults";
+import alert from "../components/Alert";
