3d1982fcd46dcf383eb9cd65ad25916c5e8d0d35
amcourson <amcourson09@sbcglobal.net>
2021-05-15 19:45:05 -0500

    updating and deploying

--- user/src/components/SearchForm/index.js
+++ user/src/components/SearchForm/index.js
@@ -6,11 +6,28 @@ function SearchForm(props) {
   return (
     <form className="search">
       <div className="form-group">
-        <label htmlFor="employee">Employee Name:</label>
-        <p>Hi this is a search form components</p>
-        </div>
-        </form>
-      
-  )};
+        <label htmlFor="User">Employee Name:</label>
+        <input
+          value={props.search}
+          onChange={props.handleInputChange}
+          name=""
+          list=""
+          type="text"
+          className="form-control"
+          placeholder="Type in a name to begin"
+          id="User"
+        />
+        <datalist id="User">
+          {props.breeds.map(User => (
+            <option value={User} key={User} />
+          ))}
+        </datalist>
+        <button type="submit" onClick={props.handleFormSubmit} className="btn btn-success">
+          Search
+        </button>
+      </div>
+    </form>
+  );
+}
 
 export default SearchForm;
--- user/src/pages/search.js
+++ user/src/pages/search.js
@@ -1,1 +1,56 @@
+import React, { Component } from "react";
+import API from "../utils/API";
+import Container from "/src/components/Container";
+import SearchForm from "../components/SearchForm";
+import SearchResults from "../components/SearchResults";
 
+
+class Search extends Component {
+  state = {
+    search: "",
+    name: [],
+    results: [],
+    error: ""
+  };
+
+  componentDidMount() {
+      API.getUserList ()
+        .then(res => this.setState({ user: res.data.message }))
+        .catch(err => console.log(err));
+    }
+  };
+
+  handleInputChange = event => {
+    this.setState({ search: event.target.value });
+  };
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+    API.getUserList(this.state.search)
+      .then(res => {
+        if (res.data.status === "error") {
+          throw new Error(res.data.message);
+        }
+        this.setState({ results: res.data.message, error: "" });
+      })
+      .catch(err => this.setState({ error: err.message }));
+  };
+  render() {
+    return (
+      <div>
+        <Container style={{ minHeight: "80%" }}>
+          <h1 className="text-center">Search By Name!</h1>
+         
+          <SearchForm
+            handleFormSubmit={this.handleFormSubmit}
+            handleInputChange={this.handleInputChange}
+            user={this.state.user}
+          />
+          <SearchResults results={this.state.results} />
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default Search;
